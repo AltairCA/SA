d8ace73b4bed5a06f88f74e99da9ca416a7a0650
Altair <alexvista1234@gmail.com>
2016-10-01 22:48:05 +0530

    initals

---
+++ SA/Migrations/201610011712425_initial userTbale.cs
@@ -0,0 +1,28 @@
+namespace SA.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialuserTbale : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        userName = c.String(),
+                        email = c.String(),
+                        password = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
---
+++ SA/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SA.Models
+{
+    class User
+    {
+        [Key]
+        public int id { get; set; }
+        public string userName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
